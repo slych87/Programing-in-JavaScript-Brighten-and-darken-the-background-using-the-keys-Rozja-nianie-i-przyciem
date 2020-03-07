cd4619ece76c10ad57daeed3e97f59b562e0b462
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-07 15:49:34 +0100

    loop if to change colors

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-/* Gotowy projekt - sprzawdz działanie strzałek i zrób podobnie. http://websamuraj.pl/examples/js/projekt4-zad2/ */
-
 let red = 100;
 let green = 100;
 let blue = 100;
@@ -7,15 +5,27 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
-  // 38 - strzałka do góry
-  // 40 - strzalka w dół
+  // console.log(e.keyCode, e.which);
 
-  // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
-  //tutaj twój kod
-  // wersja 1 - instrukcja if
-  // wersja 2 - instrukcja switch
+  //38 w gorę
+  //40 w dół
 
+  if (e.keyCode === 38) {
+    if (red < 255) {
+      red += 3;
+      green += 3;
+      blue += 3;
+    }
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
+  else if (e.keyCode === 40) {
+    if (red >= 0) {
+      red -= 3;
+      green -= 3;
+      blue -= 3;
+    }
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
 }
 
-window.addEventListener('keydown', changeColor)
+window.addEventListener('keydown', changeColor);
