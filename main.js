96f19f55837514181ce49f1cfa5d2e2e56385c5f
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-07 16:11:15 +0100

    use switch to change color

--- main.js
+++ main.js
@@ -10,21 +10,30 @@ const changeColor = (e) => {
   //38 w gorę
   //40 w dół
 
-  if (e.keyCode === 38) {
-    if (red < 255) {
-      red += 3;
-      green += 3;
-      blue += 3;
-    }
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  }
-  else if (e.keyCode === 40) {
-    if (red >= 0) {
-      red -= 3;
-      green -= 3;
-      blue -= 3;
-    }
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // if (e.keyCode === 38 && red < 255) {
+
+  //   red += 3;
+  //   green += 3;
+  //   blue += 3;
+
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // }
+  // else if (e.keyCode === 40 && red >= 0) {
+
+  //   red -= 3;
+  //   green -= 3;
+  //   blue -= 3;
+
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // }
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+      break;
   }
 }
 
